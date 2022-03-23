fdc67835ba89b7c569196ed3d72ee52f4faccbda
Jonathan Shimwell <mail@jshimwell.com>
2022-03-23 14:37:30 +0000

    added code comments

--- 2_run_openmc_dagmc_simulation.py
+++ 2_run_openmc_dagmc_simulation.py
@@ -1,5 +1,4 @@
 import openmc
-import math
 
 # Names of material tags can be found with the command line tool
 # mbsize -ll dagmc.h5m | grep 'NAME = mat:'
@@ -36,49 +35,64 @@ materials = openmc.Materials(
         mat_lower_blanket,
         mat_lower_vacuum_vessel,
         mat_upper_vacuum_vessel,
-        mat_blanket
+        mat_blanket,
     ]
 )
 
+# makes use of the dagmc geometry
 dag_univ = openmc.DAGMCUniverse("dagmc.h5m")
 
+# creates an edge of universe boundary
 vac_surf = openmc.Sphere(r=10000, surface_id=9999, boundary_type="vacuum")
 
+# specifies the region as below the universe boundary
 region = -vac_surf
 
+# creates a cell from the region and fills the cell with the dagmc geometry
 containing_cell = openmc.Cell(cell_id=9999, region=region, fill=dag_univ)
+
 geometry = openmc.Geometry(root=[containing_cell])
 
+# creates a simple isotropic neutron source in the center with 14MeV neutrons
 my_source = openmc.Source()
 my_source.space = openmc.stats.Point((0, 0, 0))
 my_source.angle = openmc.stats.Isotropic()
 my_source.energy = openmc.stats.Discrete([14e6], [1])
 
+# specifies the simulation computational intensity
 settings = openmc.Settings()
 settings.batches = 10
-settings.inactive = 0
 settings.particles = 100000
+settings.inactive = 0
 settings.run_mode = "fixed source"
 settings.source = my_source
 
-tally = openmc.Tally(name="TBR")
-tally.scores = ["(n,Xt)"]  # this catch all neutron induced tritium producing reactions
-tally.filters = [openmc.MaterialFilter(mat_blanket)]
+# adds a tally to record the number of tritium producing reactions in cells containing the blanket material
+cell_tally = openmc.Tally(name="TBR")
+cell_tally.scores = [
+    "(n,Xt)"
+]  # this catch all neutron induced tritium producing reactions
+cell_tally.filters = [openmc.MaterialFilter(mat_blanket)]
 
+# creates a mesh that covers the geometry
 mesh = openmc.RegularMesh()
 mesh.dimension = [100, 100, 100]
 mesh.lower_left = [-200, -200, -300]  # x,y,z coordinates
 mesh.upper_right = [200, 200, 300]  # x,y,z coordinates
 
+# makes a mesh tally using the previously created mesh and records tritium production on the mesh
+mesh_tally = openmc.Tally(name="tbr_on_mesh")
 mesh_filter = openmc.MeshFilter(mesh)
-mesh_tally = openmc.Tally(name='tbr_on_mesh')
 mesh_tally.filters = [mesh_filter]
-mesh_tally.scores = ['(n,Xt)']
+mesh_tally.scores = ["(n,Xt)"]
 
-tallies = openmc.Tallies([tally, mesh_tally])
+# groups the two tallies
+tallies = openmc.Tallies([cell_tally, mesh_tally])
 
+# builds the openmc model
 my_model = openmc.Model(
     materials=materials, geometry=geometry, settings=settings, tallies=tallies
 )
 
+# starts the simulation
 statepoint_file = my_model.run()
--- 3_extract_results.py
+++ 3_extract_results.py
@@ -1,24 +1,25 @@
 import openmc
+
+# this is a small package that facilitates saving of mesh tallies as vtk files
+# https://github.com/fusion-energy/openmc_mesh_tally_to_vtk
 from openmc_mesh_tally_to_vtk import write_mesh_tally_to_vtk
 
 
 # open the results file
-sp = openmc.StatePoint('statepoint.10.h5')
+sp = openmc.StatePoint("statepoint.10.h5")
 
 # access the tally using pandas dataframes
-tbr_tally = sp.get_tally(name='TBR')
-df = tbr_tally.get_pandas_dataframe()
+tbr_tally = sp.get_tally(name="TBR")
 
-tbr_tally_result = df['mean'].sum()
-tbr_tally_std_dev = df['std. dev.'].sum()
+# print cell tally results
+print(f"The tritium breeding ratio was found, TBR = {tbr_tally.mean}")
+print(f"Standard deviation on the tbr tally is {tbr_tally.std_dev}")
 
-# print results
-print('The tritium breeding ratio was found, TBR = ', tbr_tally_result)
-print('Standard deviation on the tbr tally is ', tbr_tally_std_dev)
+# extracts the mesh tally result
+tbr_mesh_tally = sp.get_tally(name="tbr_on_mesh")
 
-tbr_mesh_tally = sp.get_tally(name='tbr_on_mesh')
-
+# writes the mesh tally as a vtk file
 write_mesh_tally_to_vtk(
     tally=tbr_mesh_tally,
-    filename = "vtk_file_from_openmc_mesh.vtk",
+    filename="vtk_file_from_openmc_mesh.vtk",
 )
